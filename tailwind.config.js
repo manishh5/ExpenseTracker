6a2a34503c36116b032b27cb342891298d5779dc
Manish Dhiman <dhimanmanish472@gmail.com>
2025-10-30 12:57:44 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#6366F1",
-        success: "#16A34A",
-        danger: "#DC2626",
-        card: "#ffffff",
-        bg: "#f4f6f9",
-      },
-      boxShadow: {
-        card: "0 2px 8px rgba(0, 0, 0, 0.04)"
-      }
-    }
+    extend: {},
   },
   plugins: [],
-};
+}
